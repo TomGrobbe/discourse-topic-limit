56b0bc1b6cd6a03aa67a3165e16bf1d52be6d5f3
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-04-12 20:01:58 +0200

    Add staff excempt option

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - Currently allows up to 1 category to be limited.
 - Configureable amount of topics a user can create within that category.
 - Configureable message to be displayed when closing excess topics.
+- Configureable "ignore mode" for staff members. If enabled, then topics created by staff won't be locked.
--- plugin.rb
+++ plugin.rb
@@ -48,20 +48,23 @@ after_initialize do
 					close_message = "You already have a topic in this category. Please stick to the one and edit it if you need to change the information in that topic. Do **not** create a new topic."
 				end
 				if Category.find_by_name(target_category.to_s)
-					if Category.find_by_name(target_category.to_s).id == topic.category_id# and !user.staff?
-						dupe_post = false
-						user.topics.each do |usertopic|
-							if usertopic.category_id == topic.category_id and !usertopic.closed?
-								topics_posted += 1
-								if topics_posted > max_posts_allowed
-									dupe_post = true
+					ignore_staff = SiteSetting.discourse_topic_limit_excempt_staff
+					if (ignore_staff == true and !user.staff?) or (!ignore_staff)
+						if Category.find_by_name(target_category.to_s).id == topic.category_id
+							dupe_post = false
+							user.topics.each do |usertopic|
+								if usertopic.category_id == topic.category_id and !usertopic.closed?
+									topics_posted += 1
+									if topics_posted > max_posts_allowed
+										dupe_post = true
+									end
 								end
 							end
-						end
-						if dupe_post
-							topic.update_status("closed", true, Discourse.system_user)
-							topic.update_status("visible", false, Discourse.system_user)
-							Topic.find_by_id(topic.id).posts.last.update(raw: close_message.to_s)
+							if dupe_post
+								topic.update_status("closed", true, Discourse.system_user)
+								topic.update_status("visible", false, Discourse.system_user)
+								Topic.find_by_id(topic.id).posts.last.update(raw: close_message.to_s)
+							end
 						end
 					end
 				end
